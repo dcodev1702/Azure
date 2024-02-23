96de597a9d9faaf3737fe382011f647978fa3383
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2024-02-23 18:10:28 -0500

    Update Set-LAW-TotalRetentionDays.ps1

--- Log_Analytics/Set-LAW-TotalRetentionDays.ps1
+++ Log_Analytics/Set-LAW-TotalRetentionDays.ps1
@@ -1,12 +1,15 @@
 # Author: Chad Schultz (itoleck)
 # Source: https://github.com/itoleck/Azure/blob/3ae83d059371ba28bb6af61a4a5884fe250eb1a5/Monitor/Set-Retentions.ps1#L19
 # Modified by DCODEV1702 | 21 Feb 2024
+
+# Assumes Azure PowerShell module is installed (Install-Module -Name Az -Scope AllUsers -Force)
 param (
     [Parameter(Mandatory=$true, HelpMessage = "Resource Group name")][string] $RGName,
     [Parameter(Mandatory=$true, HelpMessage = "Log Analytics Workspace name")][string] $WorkspaceName,
     [Parameter(Mandatory=$false, HelpMessage = "Show the current table retention settings only")][boolean] $ShowOnly = $false
 )
 
+# Acquire all tables from specified LAW ($WorkspaceName)
 $tbls = Get-AzOperationalInsightsTable -ResourceGroupName $RGName -WorkspaceName $WorkspaceName
 
 if ($ShowOnly) {
@@ -14,3 +17,5 @@ if ($ShowOnly) {
 } else {
     foreach ($tbl in $tbls) {
         Update-AzOperationalInsightsTable -ResourceGroupName $RGName -WorkspaceName $WorkspaceName -TotalRetentionInDays 365 -TableName $tbl
+    }
+}
