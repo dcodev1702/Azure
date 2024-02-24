3a7f862ed11142d8c239194be54297aafb390358
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2024-02-24 00:42:30 -0500

    Update EventHub_ADX_Table_Delta.ps1

--- ADX/EventHub_ADX_Table_Delta.ps1
+++ ADX/EventHub_ADX_Table_Delta.ps1
@@ -7,10 +7,10 @@ param (
 
 # Get list of EventHubs and ADX DBase Data Connections
 $EventHubTables = Get-AzEventHub -NamespaceName $EHNamespace -ResourceGroupName $RGName | ForEach-Object { $_.Name.ToString() }
-$ADXTables = Get-AzKustoDataConnection -ResourceGroupName $RGName -ClusterName $ADXClusterName -DatabaseName $ADXDBaseName | ForEach-Object { ($_.Name -split '/')[2].ToString() -replace "-dc$"}
+$ADXDataConnections = Get-AzKustoDataConnection -ResourceGroupName $RGName -ClusterName $ADXClusterName -DatabaseName $ADXDBaseName | ForEach-Object { ($_.Name -split '/')[2].ToString() -replace "-dc$"}
 
 # Compare Both Arrays
-$uniqueInList1 = Compare-Object $EventHubTables $ADXTables | Where-Object {$_.SideIndicator -eq "<="}
+$uniqueInList1 = Compare-Object $EventHubTables $ADXDataConnections | Where-Object {$_.SideIndicator -eq "<="}
 
 # Display the delta between the two lists (arrays)
 Write-Host "Tables that are not configured in ADX Database Data Connectors"
