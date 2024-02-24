abf5143de9e1949f10767ed48ea64fb393dcbd88
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2024-02-24 00:18:32 -0500

    Update Set-LAW-TotalRetentionDays.ps1

--- Log_Analytics/Set-LAW-TotalRetentionDays.ps1
+++ Log_Analytics/Set-LAW-TotalRetentionDays.ps1
@@ -1,8 +1,18 @@
-# Author: Chad Schultz (itoleck)
-# Source: https://github.com/itoleck/Azure/blob/3ae83d059371ba28bb6af61a4a5884fe250eb1a5/Monitor/Set-Retentions.ps1#L19
-# Modified by DCODEV1702 | 21 Feb 2024
+<###########################################################################
 
-# Assumes Azure PowerShell module is installed (Install-Module -Name Az -Scope AllUsers -Force)
+    Author: Chad Schultz (itoleck)
+    Source: https://github.com/itoleck/Azure/blob/3ae83d059371ba28bb6af61a4a5884fe250eb1a5/Monitor/Set-Retentions.ps1#L19
+    Modified by DCODEV1702 | 21 Feb 2024
+
+    PRE-CONDITIONS:
+    1. Azure PowerShell module is installed (Install-Module -Name Az -Scope AllUsers -Force)
+    2. Has PowerShell or Azure CloudShell (PS) access
+    3. Has sufficient permissions to make modification to tables in specified Log Analytics Workspace (LAW)
+
+    POST-CONDITIONS:
+    1. All tables in specified LAW will have the same total retention settings (in days)
+    
+###########################################################################>
 param (
     [Parameter(Mandatory=$true, HelpMessage = "Resource Group name")][string] $RGName,
     [Parameter(Mandatory=$true, HelpMessage = "Log Analytics Workspace name")][string] $WorkspaceName,
