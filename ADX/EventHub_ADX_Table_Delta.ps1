84ed48359f8f644dbcfc2a6c43c40c34e29c26c3
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2024-02-24 00:30:24 -0500

    Update EventHub_ADX_Table_Delta.ps1

--- ADX/EventHub_ADX_Table_Delta.ps1
+++ ADX/EventHub_ADX_Table_Delta.ps1
@@ -1,12 +1,12 @@
 param (
     [Parameter(Mandatory=$true, HelpMessage = "Resource Group Name")][string] $RGName,
-    [Parameter(Mandatory=$true, HelpMessage = "Event Hub Namespace")][string] $EH-Namespace,
+    [Parameter(Mandatory=$true, HelpMessage = "Event Hub Namespace")][string] $EHNamespace,
     [Parameter(Mandatory=$true, HelpMessage = "ADX Cluster Name")][string] $ADXClusterName,
     [Parameter(Mandatory=$true, HelpMessage = "ADX Database Name")][string] $ADXDBaseName
 )
 
 # Get list of EventHubs and ADX DBase Data Connections
-$EventHubTables = Get-AzEventHub -NamespaceName $EH-Namespace -ResourceGroupName $RGName | ForEach-Object { $_.Name.ToString() }
+$EventHubTables = Get-AzEventHub -NamespaceName $EHNamespace -ResourceGroupName $RGName | ForEach-Object { $_.Name.ToString() }
 $ADXTables = Get-AzKustoDataConnection -ResourceGroupName $RGName -ClusterName $ADXClusterName -DatabaseName $ADXDBaseName | ForEach-Object { ($_.Name -split '/')[2].ToString() -replace "-dc$"}
 
 # Compare Both Arrays
