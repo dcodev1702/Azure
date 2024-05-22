0fc2c7c8c1e88b846741d114b65f298004e50d88
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2024-05-22 12:19:16 -0400

    Update Enable_EOP_Mailbox_AdvancedAuditing.ps1

--- Exchange Online/Enable_EOP_Mailbox_AdvancedAuditing.ps1
+++ Exchange Online/Enable_EOP_Mailbox_AdvancedAuditing.ps1
@@ -15,10 +15,10 @@
 # Connect to Exchange Online (uncomment the line below if you're not logged in to Exchange Online via PowerShell)
 # Connect-ExchangeOnline
 
-# Get all mailboxes within Exchange Online
+# Get all Mailboxes within Exchange Online
 $Mailboxes = (Get-Mailbox -ResultSize Unlimited -Filter { RecipientType -eq "UserMailbox" -and RecipientTypeDetails -ne "DiscoveryMailbox"}).PrimarySmtpAddress
 
-# Enable all advanced auditingo on all mailboxes
+# Enable all advanced auditing on all Exchange Online Mailboxes
 $Mailboxes | ForEach-Object {
     Write-Output $_
     Set-Mailbox -Identity $_ -AuditEnabled $true -AuditLogAgeLimit 365 -AuditAdmin @{add='Update, Copy, Move, MoveToDeletedItems, SoftDelete, HardDelete, FolderBind, SendAs, SendOnBehalf, Create, UpdateFolderPermissions, AddFolderPermissions, ModifyFolderPermissions, RemoveFolderPermissions, UpdateInboxRules, UpdateCalendarDelegation, RecordDelete, ApplyRecord, MailItemsAccessed, UpdateComplianceTag, Send, AttachmentAccess, PriorityCleanupDelete, ApplyPriorityCleanup'} -AuditDelegate @{add='Update, Move, MoveToDeletedItems, SoftDelete, HardDelete, FolderBind, SendAs, SendOnBehalf, Create, UpdateFolderPermissions, AddFolderPermissions, ModifyFolderPermissions, RemoveFolderPermissions, UpdateInboxRules, RecordDelete, ApplyRecord, MailItemsAccessed, UpdateComplianceTag, AttachmentAccess, PriorityCleanupDelete, ApplyPriorityCleanup'} -AuditOwner @{add='Update, Move, MoveToDeletedItems, SoftDelete, HardDelete, Create, MailboxLogin, UpdateFolderPermissions, AddFolderPermissions, ModifyFolderPermissions, RemoveFolderPermissions, UpdateInboxRules, UpdateCalendarDelegation, RecordDelete, ApplyRecord, MailItemsAccessed, UpdateComplianceTag, Send, SearchQueryInitiated, AttachmentAccess, PriorityCleanupDelete, ApplyPriorityCleanup'}
